033da5aa140039fa37e0b0b48129ffe1a593363c
latedude2 <simasc10@gmail.com>
2022-07-10 10:52:59 +0200

    Hide banner after pressing play

--- Assets/Scripts/UI/ShowUIElements.cs
+++ Assets/Scripts/UI/ShowUIElements.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ShowUIElements : MonoBehaviour
 {
     [SerializeField] List<GameObject> uiElements = new List<GameObject>();
+    public GameObject banner;
     public void EnableUIElements()
     {
         foreach (GameObject uiElement in uiElements)
         {
             uiElement.SetActive(true);
         }
+        banner.SetActive(false);
     }
 }
